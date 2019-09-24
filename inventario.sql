d16796a2de7edca0a3276f3444a3df7a4a8e6fe0
volkodlak300 <volkodlak300@gmail.com>
2019-09-24 14:00:31 -0500

    archivo sql para insertar

---
+++ inventario.sql
@@ -0,0 +1,6 @@
+insert into inventario(id,nombre,stock,port)
+values(101,'reloj',5,0);
+insert into inventario(id,nombre,stock,port)
+values(102,'pantalon',100,0);
+insert into inventario(id,nombre,stock,port)
+values(103,'celular',50,0);
