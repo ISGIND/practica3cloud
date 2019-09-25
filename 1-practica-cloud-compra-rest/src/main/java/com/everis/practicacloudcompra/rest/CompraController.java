f7aeb4025e3a60a29a3dd9f2ca144e62da320f86
Ivan <ivansanchez.g13@gmai.com>
2019-09-25 11:42:50 -0500

    Se quitaron depencia no ocupadas

--- 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/rest/CompraController.java
+++ 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/rest/CompraController.java
@@ -1,16 +1,13 @@
 package com.everis.practicacloudcompra.rest;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.everis.practicacloudcompra.proxy.CompraServiceProxy;
 import com.everis.practicacloudcompra.response.CompraResponse;
