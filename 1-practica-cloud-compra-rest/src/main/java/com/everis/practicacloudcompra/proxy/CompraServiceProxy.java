25513dc671f7d22318cf1e99bc9f220a158dd77d
Ivan <ivansanchez.g13@gmai.com>
2019-09-25 11:31:41 -0500

    Se agregan enotaciones ribbon y plantilla properties

--- 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/proxy/CompraServiceProxy.java
+++ 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/proxy/CompraServiceProxy.java
@@ -1,5 +1,17 @@
 package com.everis.practicacloudcompra.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.everis.practicacloudcompra.response.CompraResponse;
+
+@FeignClient(name="inventario-service")
+@RibbonClient(name = "inventario-service")
 public interface CompraServiceProxy {
+	
+	@GetMapping("/consultarInventario/producto/{id}")
+	public CompraResponse retrieveInventario(@PathVariable long id);
 
 }
--- 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/rest/CompraController.java
+++ 1-practica-cloud-compra-rest/src/main/java/com/everis/practicacloudcompra/rest/CompraController.java
@@ -3,6 +3,7 @@ package com.everis.practicacloudcompra.rest;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.everis.practicacloudcompra.proxy.CompraServiceProxy;
 import com.everis.practicacloudcompra.response.CompraResponse;
 
 @RestController
@@ -19,43 +21,29 @@ public class CompraController {
 	
 	@Value("${config.reorden}")
 	private double reorden;
+	
+	@Autowired //inyecta dependencia de service proxy
+	private CompraServiceProxy compraServiceProxy;
 
 	@GetMapping ("/consultarInventario/producto/{id}/cantidad/{cantidad}")
 	public CompraResponse consultarInventario(@PathVariable long id, 
 			@PathVariable int cantidad) {
-		
-		
-		
-		
-		String url = "http://localhost:8000/consultarInventario/producto/{id}";
-		
-		Map<String,Long> uriVariables = new HashMap<>();
-		uriVariables.put("id", id);
-		
+			
 		CompraResponse response = new CompraResponse();
 		
 		try {
-			
-			
-			ResponseEntity<CompraResponse>  respuesta =   new RestTemplate().getForEntity(url,
-					CompraResponse.class, uriVariables);
-			
-			double stock= respuesta.getBody().getValue().getStock();
+			response = compraServiceProxy.retrieveInventario(id);
+						
+			double stock= response.getValue().getStock();
 			double stockminimo = reorden/100*stock;
 			
 		 if ((stock - stockminimo) >= cantidad) {
-		
-			    response = respuesta.getBody();
 				response.setSuccessful(true);
 				response.setMessage("Se puede comparar");	
-				
-				
 		 }else {
 			 response.setSuccessful(false);
 			 response.setMessage("No se puede comprar");
-		 }
-			
-			
+		 }			
 		}catch (Exception e) {
 			response.setSuccessful(false);
 			response.setMessage(e.getMessage());
